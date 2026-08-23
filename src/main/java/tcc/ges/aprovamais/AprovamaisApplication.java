3d398e0d0b6a29b42c1b5da5c0b34fd9d9dd08c4
Leonardo Silva <levasi.leonardo55@gmail.com>
2026-08-23 14:21:19 -0300

    chore(init): setup incial do projeto com dependencias

---
+++ src/main/java/tcc/ges/aprovamais/AprovamaisApplication.java
@@ -0,0 +1,13 @@
+package tcc.ges.aprovamais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprovamaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprovamaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tcc/ges/aprovamais/AprovamaisApplicationTests.java
@@ -0,0 +1,13 @@
+package tcc.ges.aprovamais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprovamaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
