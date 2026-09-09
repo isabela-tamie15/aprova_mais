1d4d16ded295efb543f9bf0a794442b859416685
Isabela Tamie Shihara <isabelatamie@hotmail.com>
2026-09-08 23:45:27 -0300

    feat(repository): adiciona TurmaRepository, MatriculaRepository e EstagioRepository

---
+++ src/main/java/tcc/ges/aprovamais/repository/EstagioRepository.java
@@ -0,0 +1,13 @@
+package tcc.ges.aprovamais.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tcc.ges.aprovamais.entity.Estagio;
+import tcc.ges.aprovamais.entity.enums.StatusEstagio;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EstagioRepository extends JpaRepository<Estagio, Long> {
+    List<Estagio> findByStatus(StatusEstagio status);
+    Optional<Estagio> findByMatriculaId(Long matriculaId);
+}
