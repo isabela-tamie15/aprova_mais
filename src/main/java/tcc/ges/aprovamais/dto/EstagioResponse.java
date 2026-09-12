dfeb7fdcf463ab697d9adf8d0674ac388b893ea0
Isabela Tamie Shihara <isabelatamie@hotmail.com>
2026-09-12 18:09:35 -0300

    fix(dto): adiciona NoArgsConstructor e AllArgsConstructor para compatibilizar Builder com construcao via setters

--- src/main/java/tcc/ges/aprovamais/dto/EstagioResponse.java
+++ src/main/java/tcc/ges/aprovamais/dto/EstagioResponse.java
@@ -1,11 +1,17 @@
 package tcc.ges.aprovamais.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EstagioResponse {
 
     private Long id;
