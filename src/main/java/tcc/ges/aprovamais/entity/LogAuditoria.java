7dd2125d8a8e15cfed5c71803d6fb31a33c26626
Isabela Tamie Shihara <isabelatamie@hotmail.com>
2026-09-23 22:32:44 -0300

    feat(entity): adiciona entidade LogAuditoria para auditoria de acoes

---
+++ src/main/java/tcc/ges/aprovamais/entity/LogAuditoria.java
@@ -0,0 +1,49 @@
+package tcc.ges.aprovamais.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import tcc.ges.aprovamais.entity.enums.PerfilAuditoria;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "registros_auditoria")
+@Data
+@EntityListeners(AuditingEntityListener.class)
+public class LogAuditoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    @ToString.Exclude
+    private Usuario usuario;
+
+    @Column(name = "email_tentativa")
+    private String emailTentativa;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PerfilAuditoria perfil;
+
+    @Column(nullable = false)
+    private String acao;
+
+    @Column(name = "ip_origem")
+    private String ipOrigem;
+
+    @Column(columnDefinition = "TEXT")
+    private String detalhes;
+
+    @Column(nullable = false)
+    private Boolean sucesso;
+
+    @CreatedDate
+    @Column(name = "criado_em", nullable = false, updatable = false)
+    private OffsetDateTime criadoEm;
+}
